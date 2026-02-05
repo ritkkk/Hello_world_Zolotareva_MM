4cbdb882898bc8a3109d88c07be6e26c3366c2c4
ritkkk <margo.zolotareva.2007@mail.ru>
2026-02-05 17:06:52 +0300

    добавил файл mainn

---
+++ mainn.py
@@ -0,0 +1,37 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Создаем переменные 
+... name = ("Студент)")
+... a = 10
+... b = 5
+... 
+... # Математическая операция
+... result = a + b
+... 
+... # Вывод результата на экран
+... print(f"Привет, {name}!")
+... print(f"Результат сложения {a} + {b} равен {result}")
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+... name = ("Студент)")
+... a = 10
+... b = 5
+... ия
+... result = a + b
+... print(f"Привет, {name}!")
+... print(f"Результат сложения {a} + {b} равен {result}")
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+... name = ("Студент)")
+... a = 10
+... b = 5
+... result = a + b
+SyntaxError: multiple statements found while compiling a single statement
+>>> name = ("Студент)")
+>>> a = 10
+>>> b = 5
+>>> result = a + b
+>>> print(f"Привет, {name}!")
+Привет, Студент)!
+>>> print(f"Результат сложения {a} + {b} равен {result}")
+Результат сложения 10 + 5 равен 15
